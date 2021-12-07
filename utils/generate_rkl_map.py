47226ce13e36d39e3f56d189d503ef261a4c7be7
Tom Eaton <tom@tgeaton.co.uk>
2021-12-07 22:42:48 +0000

    Further updates to the RKL map adding new photos and a new kong

--- utils/generate_rkl_map.py
+++ utils/generate_rkl_map.py
@@ -7,7 +7,7 @@ import pathlib
 
 
 world = folium.Map(
-    zoom_start=2,
+    zoom_start=2.5,
     location=[13.133932434766733, 16.103938729508073]
 )
 
@@ -17,8 +17,8 @@ kongs_dir = assets_dir.joinpath('kongs')
 kong_df = pd.read_csv(assets_dir.joinpath('map_kongs.csv'), index_col=0)
 
 for kong in kong_df.reset_index().itertuples():
-    kong_icon = folium.features.CustomIcon(str(kongs_dir.joinpath(kong.kong_image_name)), icon_size=(40, 40))
-    kong_popup = f'<strong>{kong.kong_name}</strong><br>:{kong.kong_text}'
+    kong_icon = folium.features.CustomIcon(str(kongs_dir.joinpath(kong.kong_image_name)), icon_size=(30, 30))
+    kong_popup = f'<strong>{kong.kong_name}</strong><br>{kong.kong_text}'
     folium.Marker(
         location=[kong.longitude, kong.latitude],
         popup=kong.kong_name,
