fc5075b963fb9855c6c0a5c983055475e25fc0ed
Tom Eaton <tom@tgeaton.co.uk>
2021-12-07 21:54:21 +0000

    Updates to the RKL Map

--- utils/generate_rkl_map.py
+++ utils/generate_rkl_map.py
@@ -3,22 +3,27 @@
 import pandas as pd
 import folium
 
+import pathlib
+
 
 world = folium.Map(
     zoom_start=2,
     location=[13.133932434766733, 16.103938729508073]
 )
 
-# Dictionary where key is Kong name and Value is list containing Latitude / Longitude
-kong_locations = {'Roboklopp': [51.5085300, -0.1257400],
-                  }
+assets_dir = pathlib.Path(__file__).parent.parent.absolute().joinpath('assets')
+kongs_dir = assets_dir.joinpath('kongs')
 
-for ken in kong_locations.keys():
+kong_df = pd.read_csv(assets_dir.joinpath('map_kongs.csv'), index_col=0)
+
+for kong in kong_df.reset_index().itertuples():
+    kong_icon = folium.features.CustomIcon(str(kongs_dir.joinpath(kong.kong_image_name)), icon_size=(40, 40))
+    kong_popup = f'<strong>{kong.kong_name}</strong><br>:{kong.kong_text}'
     folium.Marker(
-        location=kong_locations[key],
-        popup=key,
-        tooltip=key,
-        icon=folium.Icon(color='Yellow', prefix='fa', icon='circle'),
+        location=[kong.longitude, kong.latitude],
+        popup=kong.kong_name,
+        tooltip=kong.kong_name,
+        icon=kong_icon,
     ).add_to(world)
 
 world.save('test.html')
